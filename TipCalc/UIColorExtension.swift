f520c8235bb2794ab5f90cf6c7800b3badc79613
Nicholas Naudé <nicnaude@gmail.com>
2016-03-16 15:39:26 -0700

    Make it so.

---
+++ TipCalc/UIColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtension.swift
+//  Tipster
+//
+//  Created by Nicholas Naudé on 16/03/2016.
+//  Copyright © 2016 Nicholas Naudé. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func tipsterDarkGreen() -> UIColor {
+        return  UIColor(red:0.26, green:0.49, blue:0.36, alpha:1.0)
+    }
+    
+    static func tipsterLightGreen() -> UIColor {
+        return  UIColor(red:0.50, green:0.85, blue:0.53, alpha:1.0)
+    }
+}
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -15,14 +15,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
     
+    var customersArray = []
+    var customer1 = "$0.00"
+    var customer2 = "$0.00"
+    var customer3 = "$0.00"
+    var customer4 = "$0.00"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationController!.navigationBar.barTintColor = UIColor.tipsterDarkGreen()
+        
         tipLabel.text = "Tip: $0.00"
         totalLabel.text = "Total: $0.00"
+        tipLabel.alpha = 0
+        totalLabel.alpha = 0
+        tipControl.alpha = 0
+        tableView.alpha = 0
         
+        customersArray = ["One person: \(customer1)","Two people: \(customer2)","Three people: \(customer3)","Four people: \(customer4)"]
     }
     
     // MARK: Methods
@@ -31,6 +44,10 @@ class ViewController: UIViewController {
         if (billAmountTextField.text == "") {
             tipLabel.text = "Tip: $0.00"
             totalLabel.text = "Total: $0.00"
+            tipLabel.alpha = 0
+            totalLabel.alpha = 0
+            tipControl.alpha = 0
+            tableView.alpha = 0
             
         } else {
             
@@ -38,19 +55,62 @@ class ViewController: UIViewController {
             let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
             
             let billAmount: Double! = Double(billAmountTextField.text!)
-            let tip = billAmount! * tipPercentage
-            let total = billAmount! + tip
+            let tip = round (( billAmount! * tipPercentage ) * 100) / 100
+            let total = round (( billAmount! + tip ) * 100) / 100
             
-            tipLabel.text = "Tip: $\(tip)"
+            
             totalLabel.text = "Total: $\(total)"
+            self.tipLabel.text = "Tip: $\(tip)"
             
+            customer1 = "$\(total)"
+            customer2 = "$\(total / 2)"
+            customer3 = "$\(total / 3)"
+            customer4 = "$\(total / 4)"
+            
+            tipLabel.fadeIn(0.2, delay: 0.2, completion: { (Bool) -> Void in
+                return
+            })
+            totalLabel.fadeIn(0.3, delay: 0.3, completion: { (Bool) -> Void in
+                return
+            })
+            tipControl.fadeIn(0.4, delay: 0.4, completion: { (Bool) -> Void in
+                return
+            })
+            tableView.fadeIn(0.4, delay: 0.4, completion: { (Bool) -> Void in
+                return
+            })
         }
-        
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func segmentedControl(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex
+        {
+        case 0:
+            self.tableView.reloadData()
+        case 1:
+            self.tableView.reloadData()
+        default:
+            break
+        }
     }
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
     
+    
+    // MARK: TableView functions
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell")
+        cell!.textLabel?.text = customersArray[indexPath.row] as? String
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return customersArray.count
+    }
 }
 
